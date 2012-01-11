48c53ec6d8747a930caa278d1867154a09000360
woloski <woloski@sion.com>
2012-01-11 13:50:29 -0300

    Initial Commit

---
+++ WS-Trust Login/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WS-Trust Login
+//
+//  Created by Matias Woloski on 1/11/12.
+//  Copyright Southworks 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
